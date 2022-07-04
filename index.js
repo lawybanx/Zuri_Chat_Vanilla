317d3ba3a91ba8dca139fb55d6d85ee06b85fbff
Awwal Bankole <awwalbankole@gmail.com>
2022-07-04 20:01:22 +0100

    attempt 2 at validating input

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   const input = document.querySelector('input');
   const inputValue = input.value;
-  const ul = document.querySelector('ul');
 
-  if (inputValue === null || inputValue === '') {
-    alert('Please enter a value');
-  } else {
-    const li = document.createElement('li');
-    li.textContent = inputValue;
-    ul.appendChild(li);
-    input.value = '';
-  }
+  const ul = document.querySelector('ul');
+  const li = document.createElement('li');
+  li.textContent = inputValue;
+  ul.appendChild(li);
+  input.value = '';
 });
