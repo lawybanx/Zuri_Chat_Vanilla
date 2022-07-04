3270268307e480b87138a483a399a86d03dd8959
Awwal Bankole <awwalbankole@gmail.com>
2022-07-04 19:47:10 +0100

    Started project

---
+++ index.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const input = document.querySelector('input');
+  const inputValue = input.value;
+  const ul = document.querySelector('ul');
+  const li = document.createElement('li');
+  li.textContent = inputValue;
+  ul.appendChild(li);
+  input.value = '';
+});
